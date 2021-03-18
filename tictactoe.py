7fa983284c554c43337625d6b0bf00c9ef7ad315
EliasSvedberg <51356784+EliasSvedberg@users.noreply.github.com>
2021-03-18 17:09:56 +0100

    updated logic

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,5 @@
 import pygame as pygame, sys
+import math
 
 class Game:
     def __init__(self, pygame, sys):
@@ -8,18 +9,20 @@ class Game:
         self.clock = pygame.time.Clock()
         self.winner = None
         self.draw = False
-        self.tttArray = self.create_ttt_array()
 
     class Square:
-        def __init__(self, r, c):
+        def __init__(self, r, c, xPos, yPos):
             self.row = r
             self.col = c
+            self.xPos = xPos
+            self.yPos = yPos
             self.clicked = False
 
 
-    def set_screen(self, w, h):
+    def set_screen(self, w, h, lw):
         self.width = w
         self.height = h
+        self.lineWidth = lw
         self.screen = self.pygame.display.set_mode((w,h))
         self.pygame.display.set_caption("Tic Tac Toe")
 
@@ -36,8 +39,10 @@ class Game:
         self.oImg = self.pygame.image.load(oImg)
 
         #resizing images
-        self.xImg = self.pygame.transform.scale(self.xImg, (80,80))
-        self.oImg = self.pygame.transform.scale(self.oImg, (80,80))
+        self.imgWidth = int(self.width/5)
+        self.imgHeight = int(self.height/5)
+        self.xImg = self.pygame.transform.scale(self.xImg, (self.imgWidth, self.imgHeight))
+        self.oImg = self.pygame.transform.scale(self.oImg, (self.imgWidth, self.imgHeight))
 
 
     def draw_board(self):
@@ -46,40 +51,52 @@ class Game:
         #Drawing vertical lines
         self.pygame.draw.line(self.screen,self.lineColor,
                             (self.width/3,0),
-                            (self.width/3, self.height),7)
+                            (self.width/3, self.height),self.lineWidth)
         self.pygame.draw.line(self.screen,self.lineColor,
                             (self.width/3*2,0),(self.width/3*2,
-                             self.height),7)
+                             self.height),self.lineWidth)
         #Drawing horizontal lines
         self.pygame.draw.line(self.screen,self.lineColor,
                             (0,self.height/3),
-                            (self.width, self.height/3),7)
+                            (self.width, self.height/3),self.lineWidth)
         self.pygame.draw.line(self.screen,self.lineColor,
                             (0,self.height/3*2),
-                            (self.width, self.height/3*2),7)
-
+                            (self.width, self.height/3*2),self.lineWidth)
         #Draw X and O
-
+        for r in range(3):
+            for c in range(3):
+                print(r, c, self.tttArray[r][c].clicked)
+                if (self.tttArray[r][c].clicked):
+                    self.screen.blit(self.xImg, (self.tttArray[r][c].xPos, self.tttArray[r][c].yPos))
         #Update screen
         self.pygame.display.update()
 
+
     def create_ttt_array(self):
-        #creates instances of Squares in tttArray
-        return [[self.Square(r, c) for c in range(3)] for r in range(3)]
+        #Set x,y cordinates for each img
+        xPos = (self.width/3 - self.imgWidth)/2
+        yPos = (self.height/3 - self.imgHeight)/2
+
+        #Creates instances of Squares in tttArray
+        return [[self.Square(r, c, (xPos+c*(self.width/3)),
+        (yPos+r*(self.height/3)) ) for c in range(3)] for r in range(3)]
 
     def user_click(self):
         #Get mouse pos and convert to rows/cols
         mPosX,mPosY = self.pygame.mouse.get_pos()
-        r = floor(mPosY/self.height)
-        c = floor(mPosY/self.width)
-
+        mRow = math.floor(mPosY/(self.height/3))
+        mCol = math.floor(mPosX/(self.width/3))
+        print(mPosX, mPosY)
+        print(mRow,mCol)
         #Set square to clicked
-        for sq in self.tttArray:
-            if (sq.row,sq.col) == (r,c):
-                sq.clicked = True
+        for r in range(3):
+            for c in range(3):
+                if (self.tttArray[r][c].row, self.tttArray[r][c].col) == (mRow, mCol):
+                    self.tttArray[r][c].clicked = True
 
 
     def game_loop(self):
+        self.tttArray = self.create_ttt_array()
         self.draw_board()
         #Game loop
         while (True):
@@ -88,7 +105,8 @@ class Game:
                     self.pygame.quit()
                     self.sys.exit() #Stop running
                 elif event.type == self.pygame.MOUSEBUTTONDOWN:
-                    dosomething = 1
+                    self.user_click()
+                    self.draw_board()
                     if (self.winner or self.draw):
                         endgame = 1
         #set frames/second
@@ -96,7 +114,7 @@ class Game:
         self.clock.tick(fps)
 
 game = Game(pygame, sys)
-game.set_screen(400, 400)
+game.set_screen(400, 400, 12)
 game.set_board_style((255,255,255),(0,0,0))
 game.set_fps(30)
 game.set_imgs("x.png", "o.png")
